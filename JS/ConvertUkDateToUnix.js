26760a560ecb026813885d1ee11cd1ba8741af59
hamuda.anfoune <hamuda.anfoune@cvp.com>
2021-11-04 15:39:35 +0000

    Converting date...

---
+++ JS/ConvertUkDateToUnix.js
@@ -0,0 +1,6 @@
+function convertUkDateToUnix(ukDate) {
+    var splitDate = ukDate.split('/');
+    var month = splitDate[1] - 1; //Javascript months are 0-11
+
+    return new Date(splitDate[2], month, splitDate[0]);
+}
